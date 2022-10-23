fef22ae624936e76054e34129acf34a7597a0522
Jehicoob Lopez <jehicoob.job@gmail.com>
2022-10-23 17:26:03 -0500

    Search bar added and profile picture bug fixed

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+
+  def search_all
+    @term = params[:term]
+    # raise @term.to_yaml
+    @decks = Deck.where("name like ? or description like ? and owner_id = ?", "%#{@term}%", "%#{@term}%", current_user.id)
+    @cards = Card.where("question like ? or description like ? and user_id = ?","%#{@term}%","%#{@term}%", current_user.id)
+  end
+
+  def search_code
+
+  end
+  
+end
